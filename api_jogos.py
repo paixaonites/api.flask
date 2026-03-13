702d996abd929c80823c694859274842e6812e58
paixaonites <165311798+paixaonites@users.noreply.github.com>
2026-03-13 14:28:10 -0300

    Add files via upload

--- api_jogos.py
+++ api_jogos.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -17,4 +18,5 @@ def listar_jogos():
     return jsonify (jogos)
 
 if __name__ == "__main__":
- app.run(port=5001)
+ port = int(os.environ.get("PORT", 5000))
+ app.run(host="0.0.0.0", port=port)
